e676a38f0efbb5d3ab9450cc6ac77f20ff127596
AlekseevArtem <tema9991@mail.ru>
2020-07-28 15:35:27 +0300

    4. Поиск по Places. [#294993]

--- app/src/main/java/ru/job4j/tourist/MapsActivity.java
+++ app/src/main/java/ru/job4j/tourist/MapsActivity.java
@@ -1,25 +1,37 @@
 package ru.job4j.tourist;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 import ru.job4j.tourist.store.SQLStore;
@@ -33,11 +45,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == SHOW_MARK && resultCode == RESULT_OK) {
+        if (requestCode == SHOW_MARK && resultCode == RESULT_OK) {
             int id = data.getIntExtra("mark", 0);
             Mark mark = mStore.findMarkByID(id);
             mMap.moveCamera(CameraUpdateFactory
-                    .newLatLngZoom(new LatLng(mark.getLatitude(),mark.getLongitude()), 15));
+                    .newLatLngZoom(new LatLng(mark.getLatitude(), mark.getLongitude()), 15));
         }
     }
 
@@ -46,41 +58,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         mStore = SQLStore.getInstance(this);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
         Button current = findViewById(R.id.current);
         current.setOnClickListener(this::getCurrentLocation);
         Button list = findViewById(R.id.list);
         list.setOnClickListener(this::toMarkListActivity);
-    }
-
-    private void getCurrentLocation(View view) {
-        if (mLocation != null) {
-            String title = "Hello Maps";
-            double latitude = mLocation.getLatitude();
-            double longitude = mLocation.getLongitude();
-            LatLng coordinates = new LatLng(latitude, longitude);
-            MarkerOptions marker = new MarkerOptions().position(coordinates).title(title);
-            marker.flat(true);
-            mMap.addMarker(marker);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15));
-            mStore.addMark(new Mark(latitude, longitude, title));
+        if (isMapPermissionGranted()) {
+            initMap();
         }
-    }
-
-    private void setAllMarks() {
-        mStore.getMarks().stream().forEach(mark -> {
-            LatLng coordinates = new LatLng(mark.getLatitude(), mark.getLongitude());
-            MarkerOptions marker = new MarkerOptions().position(coordinates).title(mark.getTitle());
-            marker.flat(true);
-            mMap.addMarker(marker);
-        });
-    }
-
-    private void toMarkListActivity(View view){
-        Intent intent = new Intent(this, MarkListActivity.class);
-        startActivityForResult(intent, SHOW_MARK);
     }
 
     @SuppressLint("MissingPermission")
@@ -112,5 +96,78 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Objects.requireNonNull(locationManager).
                 requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, loc);
+        Places.initialize(this, getString(R.string.google_maps_key));
+        AutocompleteSupportFragment search = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        search.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+        search.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                addNewMark(place.getLatLng(), place.getName());
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                Log.i("MainActivity", "An error occurred: " + status);
+            }
+        });
+    }
+
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    private boolean isMapPermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1 &&
+                grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            initMap();
+        }
+    }
+
+    private void getCurrentLocation(View view) {
+        if (mLocation != null) {
+            String title = "Hello Maps";
+            LatLng coordinates = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
+            addNewMark(coordinates, title);
+        }
+    }
+
+    private void addNewMark(LatLng coordinates, String title) {
+        MarkerOptions marker = new MarkerOptions().position(coordinates).title(title);
+        marker.flat(true);
+        mMap.addMarker(marker);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15));
+        mStore.addMark(new Mark(coordinates.latitude, coordinates.longitude, title));
+    }
+
+    private void setAllMarks() {
+        mStore.getMarks().stream().forEach(mark -> {
+            LatLng coordinates = new LatLng(mark.getLatitude(), mark.getLongitude());
+            MarkerOptions marker = new MarkerOptions().position(coordinates).title(mark.getTitle());
+            marker.flat(true);
+            mMap.addMarker(marker);
+        });
+    }
+
+    private void toMarkListActivity(View view) {
+        Intent intent = new Intent(this, MarkListActivity.class);
+        startActivityForResult(intent, SHOW_MARK);
     }
 }
